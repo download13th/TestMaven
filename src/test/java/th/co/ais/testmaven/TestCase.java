07c209189356e256753340f8fadd170d7ad52e4c
unknown <chinnawb@NBAHP1510119.corp.ais900.org>
2015-10-15 17:24:12 +0700

    Add Test Case

---
+++ src/test/java/th/co/ais/testmaven/TestCase.java
@@ -0,0 +1,14 @@
+package th.co.ais.testmaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
